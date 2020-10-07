48df3cea6583517cfc4ca55eac1ed621d6102911
Long Huynh <Longhuynh741@yahoo.com>
2020-10-06 20:00:33 -0400

    added startTracker and inquirer prompts

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 var mysql = require("mysql");
 var inquirer = require("inquirer");
+const { listenerCount } = require("process");
 
 var connection = mysql.createConnection({
   host: "localhost",
@@ -17,5 +18,42 @@ var connection = mysql.createConnection({
 
 connection.connect(function(err) {
   if (err) throw err;
-  console.log("connected as id " + connection.threadId);
+    console.log("connected as id " + connection.threadId);
+    startTracker();
 });
+
+function startTracker() {
+    inquirer
+        .promt({
+            name: "start",
+            message: "What would you like to do?",
+            type: list,
+            choices: [
+                "View All Employees",
+                "View All Employees by Department",
+                "View All Employees by Role",
+                "Add Employee",
+                "Add Department",
+                "EXIT",
+            ],
+        }).then(function (answer) {
+            if (answer.start === "View All Employees") {
+                allEmployees();
+            }
+            else if (answer.start === "View All Employees by Department") {
+                allEmployeesByDepartment();
+            }
+            else if (answer.start === "View All Employees by Role") {
+                allEmployeesByRole();
+            }
+            else if (answer.start === "Add Employee") {
+                addEmployee();
+            }
+            else if (answer.start === "Add Department") {
+                addDepartment();
+            }
+            else if (answer.start === "EXIT") {
+                connection.end
+            }
+        });
+}
